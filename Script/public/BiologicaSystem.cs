351c069426318f944497b3865b168798ec8b68a0
abab5601 <abab5601@yahoo.com.tw>
2018-12-21 16:16:25 +0800

    Update BiologicaSystem.cs

--- Script/public/BiologicaSystem.cs
+++ Script/public/BiologicaSystem.cs
@@ -12,14 +12,62 @@ public class BiologicaSystem : MonoBehaviour {
     public User.money money;
     public User.control[] Control;
     public User.Backpack[] Backpacks;
-    public User.anim Anim;
-	// Use this for initialization
+    public User.anim[] Anim;
 	void Start () {
+		 
+	}
+    void Update () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    /// <summary>
+    /// 變動血量
+    /// </summary>
+    /// <param name="HP">變動血量數值</param>
+    public void hp(int HP)
+    {
+
+    }
+    /// <summary>
+    /// 變動魔力
+    /// </summary>
+    /// <param name="HP">變動魔力數值</param>
+    public void mp(int MP)
+    {
+
+    }
+    /// <summary>
+    /// 控制
+    /// </summary>
+    /// <param name="control">控制資料</param>
+    /// /// <param name="OUT">增加 = true 減少 = false </param>
+    public void control(User.control[] control,bool OUT)
+    {
+
+    }
+    /// <summary>
+    /// 撿起物品
+    /// </summary>
+    /// <param name="backpack">物品DATA</param>
+    /// <param name="OUT">增加 = true 減少 = false </param>
+    public void backpack(User.Backpack[] backpack, bool OUT)
+    {
+
+    }
+    /// <summary>
+    /// 學習動作
+    /// </summary>
+    /// <param name="anim">動作</param>
+    /// <param name="OUT">增加 = true 減少 = false </param>
+    public void anim(User.anim[] anim, bool OUT)
+    {
+
+    }
+    /// <summary>
+    /// 錢
+    /// </summary>
+    /// <param name="money">金額</param>
+    public void Money(User.money money)
+    {
+
+    }
 }
