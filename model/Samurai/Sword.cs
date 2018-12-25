b884bd8882b0e77d899e5d827d3ed0cba7fa1eaf
kkyy9043 <kkyyuli@gmail.com>
2018-12-25 14:30:52 +0800

    扣血

---
+++ model/Samurai/Sword.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public Prop prop;
+    private void Awake()
+    {
+        prop = GetComponent<Prop>();
+       
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+      if(collision.gameObject.tag=="player")
+        { }
+      else if(collision.gameObject.tag=="NPC")
+        collision.gameObject.GetComponent<BiologicaSystem>().hp(-10);
+    }
+}
