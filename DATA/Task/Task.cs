190051a6b77db9e386e58c97fbebb573af72cd8a
abab5601 <abab5601@yahoo.com.tw>
2019-02-06 01:12:06 +0800

    TAsk

---
+++ DATA/Task/Task.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 主線任務DataBase + 格式
+/// </summary>
+[CreateAssetMenu(fileName = "Tack", menuName = "DATA/任務", order = 3)]
+public class Task : ScriptableObject
+{
+    [Header("主線任務清單")]
+    public task[] Tasks;
+    [System.Serializable]
+    public struct task
+    {
+        [Header("")]
+        public string Name;
+        [Header("")]
+        public string Description;
+        [Header("")]
+        public string[] condition;
+        [Header("")]
+        public Object script;
+    }
+}
