8d09e6ab5c274ee5824b89ee1b6834d751e114b5
abab5601 <abab5601@yahoo.com.tw>
2019-02-08 12:30:02 +0800

    stask

--- DATA/Task/Task.cs
+++ DATA/Task/Task.cs
@@ -2,23 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 /// <summary>
-/// 主線任務DataBase + 格式
+/// 任務DataBase + 格式
 /// </summary>
 [CreateAssetMenu(fileName = "Tack", menuName = "DATA/任務", order = 3)]
 public class Task : ScriptableObject
 {
+    /// <summary>
+    /// 主線任務
+    /// </summary>
     [Header("主線任務清單")]
-    public task[] Tasks;
+    public List<ITask> main;
+    /// <summary>
+    /// 支線任務
+    /// </summary>
+    [Header("支線任務")]
+    public List<ITask> Branch_line;
+    /// <summary>
+    /// 限時任務
+    /// </summary>
+    [Header("限時任務")]
+    public List<Time_limited> time_limited;
+    /// <summary>
+    /// 限時任務格式
+    /// </summary>
     [System.Serializable]
-    public struct task
+    public struct Time_limited
     {
-        [Header("")]
-        public string Name;
-        [Header("")]
-        public string Description;
-        [Header("")]
-        public string[] condition;
-        [Header("")]
-        public Object script;
+        [Header("任務腳本")]
+        public ITask Task;
+        [Header("限時任務結束倒數")]
+        public float Time_limited_time;
+        [Header("任務再次開始時間(24小時制 格式 : (HHmm) HH 時 mm 分)")]
+        [Header("再次開起時會將腳本刪除重新add")]
+        [Header("沒有要再次開啟可空")]
+        public string[] time;
     }
 }
