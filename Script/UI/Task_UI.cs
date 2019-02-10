9dbd91c05e5137a8b931745302186c1bdeabac87
SangSang_na <mingming12369852@gmail.com>
2019-02-10 18:52:48 +0800

    ui更動

--- Script/UI/Task_UI.cs
+++ Script/UI/Task_UI.cs
@@ -1,44 +1,106 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Task_UI : MonoBehaviour {
-    #region
-    [System.Serializable]
-    public struct UI
+
+public class Task_ui : MonoBehaviour {
+    //0=未完成 1=進行中 2=已完成
+
+    public TaskData Td;
+    public Text[] name, text, but;
+    public string e;
+
+
+
+    public void Update()
     {
-        public Text Name;
-        public Text Description;
-        public box[] boxs;
-        public Button button;
+
+        switch (Td.UI_number) {
+
+            case 0:
+                a();
+                break;
+            case 1:
+                b();
+                break;
+            case 2:
+                c();
+                break;
+
+
+
+        }
+
+
     }
-    [System.Serializable]
-    public struct box
-    {
-        public Image image;
-        public Text text;
+    void a() {
+        for (int i = 0; i <= 10; i++)
+        {
+
+            name[i].text = Td.BT[i].Name;
+            text[i].text = Td.BT[i].Description;
+
+            switch (Td.BT[i].Schedule)
+            {
+                case 0:
+                    but[i].text = "未完成";
+                    break;
+                case 1:
+                    but[i].text = "進行中";
+                    break;
+                case 2:
+                    but[i].text = "已完成";
+                    break;
+            }
+
+        }
     }
-    #endregion
-    public UI ui;
-    public TaskData taskData;
-    public GameObject not, objs;
-    public List<GameObject> gameObjects;
-    // Use this for initialization
-    public int X, Y;
-	// Update is called once per frame
-    /// <summary>
-    /// 任務種類
-    /// </summary>
-    /// <param name="X">主線任務,之線任務,即時任務</param>
-    public void x(int X) { this.X = X; }
-    /// <summary>
-    /// 任務狀態
-    /// </summary>
-    /// <param name="Y">未接收,進行中,已完成</param>
-    public void y(int Y) { this.Y = Y; }
-    private int arrty;
-    void Update () {
-		
-	}
+    void b() {
+        for (int i = 0; i <= 10; i++)
+        {
+
+            name[i].text = Td.VT[i].Name;
+            text[i].text = Td.VT[i].Description;
+
+            switch (Td.VT[i].Schedule)
+            {
+                case 0:
+                    but[i].text = "未完成";
+                    break;
+                case 1:
+                    but[i].text = "進行中";
+                    break;
+                case 2:
+                    but[i].text = "已完成";
+                    break;
+            }
+
+        }
+    }
+    void c() {
+        for (int i = 0; i <= 10; i++)
+        {
+
+            name[i].text = Td.AT[i].Name;
+            text[i].text = Td.AT[i].Description;
+
+            switch (Td.AT[i].Schedule)
+            {
+                case 0:
+                    but[i].text = "未完成";
+                    break;
+                case 1:
+                    but[i].text = "進行中";
+                    break;
+                case 2:
+                    but[i].text = "已完成";
+                    break;
+            }
+
+        }
+    }
+    //
+
 }
